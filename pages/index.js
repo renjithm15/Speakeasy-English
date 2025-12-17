4084ad8bac0c6c87a4e1e777a0fa9fa5c3f9cb98
renjithm15 <71491984+renjithm15@users.noreply.github.com>
2025-12-18 00:09:06 +0530

    Create Home component for the main page

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>SpeakEasy English</h2>
+      <p>Welcome! App setup is successful.</p>
+      <p>Malayalam English speaking app coming soon.</p>
+    </div>
+  );
+}
