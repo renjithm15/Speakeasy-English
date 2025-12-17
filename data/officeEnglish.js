19d35ce8656861a0b256bb0348c99c07bc6deff9
renjithm15 <71491984+renjithm15@users.noreply.github.com>
2025-12-18 01:01:06 +0530

    Add officeEnglish.js with common phrases

---
+++ data/officeEnglish.js
@@ -0,0 +1,20 @@
+const officeEnglish = [
+  {
+    ml: "ഈ ജോലി ഇന്ന് തീർക്കാം",
+    en: "I will complete this work today"
+  },
+  {
+    ml: "ദയവായി കുറച്ച് സമയം തരൂ",
+    en: "Please give me some time"
+  },
+  {
+    ml: "ഞാൻ മീറ്റിംഗിൽ പങ്കെടുക്കും",
+    en: "I will attend the meeting"
+  },
+  {
+    ml: "ഇത് ഞാൻ ഉടൻ അയക്കും",
+    en: "I will send this shortly"
+  }
+];
+
+export default officeEnglish;
